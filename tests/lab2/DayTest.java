3f2c86f0ccfdca6ee54c2c62d9fb690345d5824b
baronvladziu <baronvladziu@gmail.com>
2018-10-15 10:01:15 +0200

    Added DayTest

---
+++ tests/lab2/DayTest.java
@@ -0,0 +1,46 @@
+package lab2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DayTest {
+
+    private Day day = Day.MON;
+
+    @Test
+    void nextDay() {
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.TUE);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.WEN);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.THU);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.FRI);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.SAT);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.SUN);
+        this.day = this.day.nextDay();
+        assertEquals(this.day, Day.MON);
+    }
+
+    @Test
+    void prevDay() {
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.SUN);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.SAT);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.FRI);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.THU);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.WEN);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.TUE);
+        this.day = this.day.prevDay();
+        assertEquals(this.day, Day.MON);
+    }
+}
